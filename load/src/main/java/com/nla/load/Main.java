5410412a533c2fc592b77c2225057b3365e586af
Sylvain Juge <syl20j@gmail.com>
2014-07-31 08:59:37 +0200

    handle proper shutdown

--- load/src/main/java/com/nla/load/Main.java
+++ load/src/main/java/com/nla/load/Main.java
@@ -6,16 +6,31 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
 
     public static final String BASE_URL = "http://dev1/projectPlace/";
 
+    private static AtomicBoolean stop = new AtomicBoolean(false);
+
     public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                stop.set(true);
+                System.out.println("Shutting down");
+            }
+        });
 
-        doRequest(BASE_URL);
-
-        System.out.println("Done !");
+        while (!stop.get()) {
+            doRequest(BASE_URL);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static void doRequest(String url) {
