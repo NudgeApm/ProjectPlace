8d2f5930b0b9c096ece7d840ef7cb992552d6854
Sylvain Juge <syl20j@gmail.com>
2014-08-07 11:13:18 +0200

    simplify kill cmd & truncate output

--- load/longrun.sh
+++ load/longrun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dir=$(dirname $0)
 
-jps | grep project-place-load | awk '{print $1}' | xargs -i{} kill {} 2>&1 /dev/null
+jps | grep 'project-place-load' | awk '{print $1}' | xargs kill
+echo '' > nohup.out
 nohup java -jar ${dir}/target/*-jar-with-dependencies.jar $@ &
