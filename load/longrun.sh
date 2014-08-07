52aa950443434a579c06593ebcf0667d9208e771
Sylvain Juge <syl20j@gmail.com>
2014-08-07 11:16:39 +0200

    fix remove nohup.out file

--- load/longrun.sh
+++ load/longrun.sh
@@ -2,5 +2,5 @@
 dir=$(dirname $0)
 
 jps | grep 'project-place-load' | awk '{print $1}' | xargs kill
-echo '' > nohup.out
+rm ./nohup.out
 nohup java -jar ${dir}/target/*-jar-with-dependencies.jar $@ &
